a07aa906303d9bfc31d6051120e4b8de6bb04a1f
TMV <m.timarsuev@gmail.com>
2022-07-11 19:00:50 +0300

    Week13_done

--- Sem5/CM2030_GraphicsProgramming/Week13/softwarMirror-Template/sketch.js
+++ Sem5/CM2030_GraphicsProgramming/Week13/softwarMirror-Template/sketch.js
@@ -1,35 +1,44 @@
 var video;
 
 function setup() {
-    createCanvas(640 * 2, 480);
-    pixelDensity(1);
-    video = createCapture(VIDEO);
-    video.hide();
-    noStroke();
-    angleMode(DEGREES);
+  createCanvas(640 * 2, 480);
+  pixelDensity(1);
+  video = createCapture(VIDEO);
+  video.hide();
+  noStroke();
+  angleMode(DEGREES);
+  rectMode(CENTER);
 }
 
 function draw() {
-    background(0);
-    image(video, 0, 0);
+  background(0);
+  image(video, 0, 0);
 
-    video.loadPixels();
+  video.loadPixels();
 
-    translate(640, 0);
-    var arcSize = 20;
-    translate(arcSize / 2, arcSize / 2);
+  translate(640, 0);
+  var arcSize = 20;
+  translate(arcSize / 2, arcSize / 2);
 
-    for (/*your code here*/) {
-        for (/*your code here*/) {
+  for (let i = 0; i < video.height; i += arcSize) {
+    for (let j = 0; j < video.width; j += arcSize) {
+      // your code here
+      let pxInd = (j * video.width + i) * 4;
+      let r = video.pixels[pxInd];
+      let g = video.pixels[pxInd + 1];
+      let b = video.pixels[pxInd + 2];
+      let a = video.pixels[pxInd + 3];
 
-            // your code here
+      let pixelBrightness = brightness(color(r, g, b, a));
+      push();
+      translate(i, j);
 
-            push();
-            translate(x, y);
-            var theta = map(pixelBrightness, 0, 255, 0, 360);
-            rotate((180 - theta) / 2);
-            arc(0, 0, arcSize, arcSize, 0, theta);
-            pop();
-        }
+      // var theta = map(pixelBrightness, 0, 255, 0, 360);
+      // rotate((180 - theta) / 2);
+      // arc(0, 0, arcSize, arcSize, 0, theta);
+      let size = map(pixelBrightness, 0, 255, 0, arcSize);
+      rect(0, 0, size, size);
+      pop();
     }
+  }
 }
